df18f76d4d1bfc7b5c269a1b0d38cebc4ae3e904
Yao Lu <luyaocns@gmail.com>
2020-05-14 21:47:32 +0800

    paramgrid -> gridparams

--- src/MLSuiteBase.jl
+++ src/MLSuiteBase.jl
@@ -2,9 +2,10 @@ __precompile__(true)
 
 module MLSuiteBase
 
+using JSON, SHA, Glob
 import ScikitLearnBase: is_classifier
 
-export paramgrid, reset!, istree, isrnn, is_ranker, support_multiclass, signone, modelhash
+export @grid, gridparams, reset!, istree, isrnn, is_ranker, support_multiclass, signone, modelhash
 
 reset!(m) = nothing
 istree(m) = false
@@ -13,8 +14,12 @@ support_multiclass(m) = is_classifier(m)
 is_ranker(m) = false
 modelhash(m) = hash(m)
 
-paramgrid(grid::AbstractDict, combine = Iterators.product) =
-    [Dict(zip(keys(grid), v)) for v in combine(values(grid)...)]
+gridparams(d::AbstractDict, combine = Iterators.product) =
+    [Dict(zip(keys(d), v)) for v in combine(values(d)...)]
+
+gridparams(d::AbstractArray) = mapreduce(vec ∘ gridparams, vcat, d)
+
+gridparams(x) = x
 
 Base.sign(x::Real, Θ) = ifelse(x < -Θ, oftype(x, -1), ifelse(x > Θ, one(x), zero(x)))
 
@@ -22,6 +27,65 @@ function signone(x::Real, Θ = zero(x))
     y = sign(x, Θ)
     !iszero(y) && return y
     rand([-one(x), one(x)])
+end
+
+function embed_include(file, mod)
+    if isfile(file)
+        lines = readlines(file)
+    else
+        lines = split(file, '\n')
+    end
+    for (n, line) in enumerate(lines)
+        startswith(line, '#') && continue
+        m = match(r"(?<=include\().*?(?=\))", line)
+        isnothing(m) && continue
+        src = mod.eval(Meta.parse(m.match))
+        src = isabspath(src) ? src : joinpath(dirname(file), src)
+        lines[n] = embed_include(src, mod)
+    end
+    join(lines, '\n')
+end
+
+macro grid(n, ex)
+    params = __module__.eval(ex)
+    file = string(__source__.file)
+    if !isfile(file)
+        return params[1]
+    end
+    line = __source__.line
+    lines = split(embed_include(file, __module__), '\n')
+    root = "job_" * splitext(basename(file))[1]
+    project = "#!/bin/bash\nexport JULIA_PROJECT=$(Base.load_path()[1])\n"
+    for param in rand(params, n)
+        dir = mkpath(joinpath(root, bytes2hex(sha1(json(param)))))
+        open(joinpath(dir, "param.json"), "w") do io
+            JSON.print(io, param, 4)
+        end
+        write(joinpath(dir, "julia.sh"), project, raw"""
+        source ~/.bashrc
+        cd $(dirname $0)
+        [ -f done ] && exit 0
+        julia main.jl 2>&1 | tee julia.out
+        [ $? == 0 ] && touch done
+        """)
+        write(joinpath(dir, "mpi.sh"), project, raw"""
+        cd $(dirname $0)
+        [ -f done ] && exit 0
+        mpirun julia main.jl 2>&1 | tee mpi.out
+        [ $? == 0 ] && touch done
+        """)
+        write(joinpath(dir, "slurm.sh"), project, raw"""
+        cd $(dirname $0)
+        [ -f done ] && exit 0
+        srun julia main.jl
+        [ $? == 0 ] && touch done
+        """)
+        var_name = match(r"(.+?)(?=\s+\=)", lines[line]).match
+        lines[line] = "$var_name = $param"
+        write(joinpath(dir, "main.jl"), join(lines, '\n'))
+        foreach(sh -> chmod(sh, 0o775), glob("*.sh", dir))
+    end
+    return :(exit())
 end
 
 end
